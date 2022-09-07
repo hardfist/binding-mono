fdc7c0a726cf3bd623de9c79e41174294765984a
hardfist <yangjianzju@gmail.com>
2022-09-07 21:28:06 +0800

    chore: simplify macro

--- binding_options/src/lib.rs
+++ binding_options/src/lib.rs
@@ -5,55 +5,24 @@ use napi_derive::napi;
 use napi::bindgen_prelude::ToNapiValue;
 
 use serde::{Deserialize,Serialize};
-#[cfg(not(feature="node-api"))]
-#[derive(Debug,Default)]
-#[derive(Deserialize,Serialize)]
+
+#[cfg_attr(feature="node-api",napi)]
+#[derive(Debug,Default,Deserialize,Serialize)]
 pub enum Mode {
     #[default]
     DEVELOPMENT,
     PRODUCTION
 }
 
-#[cfg(feature="node-api")]
-#[derive(Debug,Default)]
-#[napi]
-pub enum Mode {
-    #[default]
-    DEVELOPMENT,
-    PRODUCTION
-}
-
-#[cfg(not(feature="node-api"))]
-#[derive(Debug,Default)]
-#[derive(Deserialize,Serialize)]
+#[cfg_attr(feature="node-api",napi)]
+#[derive(Debug,Default,Deserialize,Serialize)]
 pub enum Target {
     #[default]
     ES3,
     ES5
 }
-#[cfg(feature="node-api")]
-#[derive(Debug,Default)]
-#[napi]
-pub enum Target {
-    #[default]
-    ES3,
-    ES5
-}
-
-
-
-
-#[cfg(feature="node-api")]
-#[derive(Debug,Default)]
-#[napi(object)]
-pub struct CompilerOptions {
-   pub mode: Mode,
-   pub targets: Vec<Target>,
-   pub context: String
-}
-
-#[cfg(not(feature="node-api"))]
-#[derive(Deserialize,Debug,Default,Serialize)]
+#[cfg_attr(feature="node-api",napi(object))]
+#[derive(Debug,Default,Deserialize,Serialize)]
 pub struct CompilerOptions {
    #[serde(default)]
    pub mode: Mode,
