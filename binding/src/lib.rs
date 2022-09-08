8ce3661e6472df1f521a43e194d848698a7409fe
hardfist <yangjianzju@gmail.com>
2022-09-08 18:13:45 +0800

    chore: add bug

--- binding/src/lib.rs
+++ binding/src/lib.rs
@@ -1,31 +1,13 @@
 #![deny(clippy::all)]
-use napi::bindgen_prelude::{Promise, Buffer};
+use napi::bindgen_prelude::{*};
 use rspack_core::{options::{BindingOptions}, callback};
 use napi::{Result,Error, JsFunction};
 use napi_derive::napi;
 use napi::{threadsafe_function::{ErrorStrategy, ThreadsafeFunction, ThreadsafeFunctionCallMode}};
 use napi::threadsafe_function::ThreadSafeResultContext;
 #[napi]
-pub fn build(options:BindingOptions) -> Result<()>{
-  let loader: JsFunction = options.module.binding_uses.and_then(|x| x.loader).ok_or("err").map_err(|err| Error::new(napi::Status::GenericFailure, "failed".to_owned()))?;
-  let tsfn: ThreadsafeFunction<u32, ErrorStrategy::CalleeHandled> = loader
-    .create_threadsafe_function(0, |ctx| {
-      ctx.env.create_uint32(ctx.value + 1).map(|v| vec![v])
-    },|ctx: ThreadSafeResultContext<Promise<Buffer>>| {
-                      let return_value = ctx.return_value;
-
-                      ctx
-                        .env
-                        .execute_tokio_future(
-                          async move {
-                            Ok(())
-                          },
-                          |_env, ret| Ok(ret),
-                        )
-                        .expect("failed to execute tokio future");
-                    },)?;
-
-   tsfn.call(Ok(100), ThreadsafeFunctionCallMode::Blocking);
-
-  Ok(())
+pub fn build(env:napi::Env,options:BindingOptions) -> napi::JsUndefined{
+  let loader: JsFunction = options.module.binding_uses.and_then(|x| x.loader).ok_or("err").map_err(|err| Error::new(napi::Status::GenericFailure, "failed".to_owned())).unwrap();
+  loader.call(None, &[env.create_int32(2).unwrap()]);
+  env.get_undefined().unwrap()
 }
